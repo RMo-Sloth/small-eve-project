db15d960a49bfac7bbfec2bfb3cfcda4f3b3c77a
Rob Monhemius <robmonhemius@gmail.com>
2021-06-24 22:44:33 +0200

    import d3

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('graph') graph !: ElementRef<HTMLElement>;
+  data: Array<{x:number, y: number}> = [
+    {x: 100, y: 200},
+    {x: 200, y: 300},
+    {x: 300, y: 200},
+    {x: 150, y: 125},
+    {x: 400, y: 500},
+  ]
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    const height = this.graph.nativeElement.clientHeight;
+    const width = this.graph.nativeElement.clientWidth;
+    const scale_x = d3.scaleLinear()
+      .domain( [0, 500] )
+      .range( [0, width] )
+    const scale_y = d3.scaleLinear()
+      .domain( [0, 500] )
+      .range( [height, 0] )
 
-  ngOnInit(): void {
+    d3.select( this.graph.nativeElement )
+      .selectAll( "circle" )
+      .data( this.data )
+      .enter()
+      .append( "circle" )
+      .attr( "r", 5 ).attr( "fill", "red" )
+      .attr( "cx", d => scale_x( d.x ) )
+      .attr( "cy", d => scale_y( d.y ) );
   }
 
 }
