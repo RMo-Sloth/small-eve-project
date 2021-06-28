317b069933f6a66c5d173b4b8826bee4d39d70ae
Rob Monhemius <robmonhemius@gmail.com>
2021-06-28 02:05:34 +0200

    abstracted update_chart logic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,18 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
-import { FwEmpiresService } from 'src/app/fw-empires.service';
+import { EmpireData, FwEmpiresService } from 'src/app/fw-empires.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit, OnInit {
+export class HomeComponent implements AfterViewInit {
   @ViewChild('graph') graph !: ElementRef<HTMLElement>;
-  data: Array<{x:number, y: number}> = [
-    {x: 100, y: 200},
-    {x: 200, y: 300},
-    {x: 300, y: 200},
-    {x: 150, y: 125},
-    {x: 400, y: 500},
-  ]
+  @ViewChild('chart') chart !: ElementRef<HTMLElement>;
+  @ViewChild('legend') legend !: ElementRef<HTMLElement>;
+
+  private data !: EmpireData[] | null;
 
   constructor(
     private empire_service: FwEmpiresService
@@ -23,28 +20,100 @@ export class HomeComponent implements AfterViewInit, OnInit {
 
   }
 
-  ngOnInit() {
-    this.empire_service.data$.subscribe( console.log );
-  }
-
   ngAfterViewInit(): void {
-    const height = this.graph.nativeElement.clientHeight;
-    const width = this.graph.nativeElement.clientWidth;
-    const scale_x = d3.scaleLinear()
-      .domain( [0, 500] )
-      .range( [0, width] )
-    const scale_y = d3.scaleLinear()
-      .domain( [0, 500] )
-      .range( [height, 0] )
-
-    d3.select( this.graph.nativeElement )
-      .selectAll( "circle" )
-      .data( this.data )
-      .enter()
-      .append( "circle" )
-      .attr( "r", 5 ).attr( "fill", "red" )
-      .attr( "cx", d => scale_x( d.x ) )
-      .attr( "cy", d => scale_y( d.y ) );
+    this.empire_service.data$.subscribe( data => {
+      this.data = data;
+      if( data ) this.update_svg();
+    });
   }
+
+  update_svg() {
+    this.update_chart();
+  }
+
+  private update_chart() {
+    if( this.data === null ) return;
+
+    const chart = new BarChartMeta( new SystemsControlled( this.data ) );
+
+    d3.select( this.chart.nativeElement )
+    .selectAll('rect')
+    .data( this.data )
+    .enter()
+    .append( 'rect' )
+    .attr('width', (d, i) => chart.x_scale(d, i) )
+    .attr( 'x', (d, i) => i*170 )
+    .attr( 'y', d => chart.y_pos( d.systems_controlled ) )
+    .attr('height', d => chart.y_scale( d.systems_controlled ) )
+    .attr( 'fill', d => d.color )
+  }
+
+}
+
+// classes
+class BarChartMeta {
+  private area = new SvgArea( 0, 379, 621, 1000 );
+  constructor(
+    private data: DataExtractor
+  ) {}
+
+  public get y_scale(): d3.ScaleLinear<number, number, never> {
+    return d3.scaleLinear().domain([0, this.data.max]).range([ 0, this.area.height ]);
+  }
+
+  public y_pos( d: any ): any {
+    return this.area.bottom - this.y_scale( d );
+  }
+
+  public x_scale( d: any, index: number ): number {
+    return 150;
+  }
+
+}
+
+
+abstract class DataExtractor {
+  public abstract max: number;
+  public abstract min: number;
+
+  constructor( protected data: any[] ){}
+}
+
+class SystemsControlled extends DataExtractor {
+  constructor(
+    data: EmpireData[]
+  ){
+    super( data )
+  }
+
+  public get max(): number {
+    return d3.max( this.data, d => d.systems_controlled ) as number;
+  }
+
+  public get min() {
+    return d3.min( this.data, d => d.systems_controlled ) as number;
+  }
+
+}
+
+class SvgArea {
+  private padding: number = 20;
+
+  constructor(
+    private x1: number,
+    private y1: number,
+    private x2: number,
+    private y2: number
+  ) {}
+
+    public get width(): number {
+      return this.x2 - this.x1 - 2 * this.padding;
+    }
+    public get height(): number {
+      return this.y2 - this.y1 - 2 * this.padding;
+    }
+    public get bottom() {
+      return this.y2 - this.padding;
+    }
 
 }
