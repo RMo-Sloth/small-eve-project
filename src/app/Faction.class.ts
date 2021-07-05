d8f7a1b8a0ae52e5701b3f660c23c9d28b95f4ee
Rob Monhemius <robmonhemius@gmail.com>
2021-07-05 18:44:22 +0200

    removed constructor + created FactionDataAdapter

--- src/app/Faction.class.ts
+++ src/app/Faction.class.ts
@@ -1,43 +1,49 @@
+import { RawEmpireData } from "./fw-empires.service";
+
 export class Faction {
   public name !: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente';
   public color !: string;
   public enabled: boolean = true;
-
-  constructor() {}
-}
+  public data!: FactionDataAdapter;
+} // todo add constructor with rawdata
 
 export class MinmatarFaction extends Faction {
   public color = '#653834';
-
-  constructor() {
-    super();
-    this.name = 'Minmatar';
-  }
+  public name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' = 'Minmatar';
 }
 
 export class CaldariFaction extends Faction {
   public color = '#4a6c7f';
-
-  constructor() {
-    super();
-    this.name = 'Caldari';
-  }
+  public name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' = 'Caldari';
 }
 
 export class AmarrFaction extends Faction {
   public color = '#7f6c50';
-
-  constructor() {
-    super();
-    this.name = 'Amarr';
-  }
+  public name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' = 'Amarr';
 }
 
 export class GallenteFaction extends Faction {
   public color = '#366565';
+  public name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' = 'Gallente';
+}
 
-  constructor() {
-    super();
-    this.name = 'Gallente';
+
+
+// Faction data select
+class FactionDataAdapter {
+  private _systems_controlled: number;
+  private _pilots: number;
+  private _vicory_points: { yesterday: number; last_week: number; total: number; }
+  private _kills: { yesterday: number; last_week: number; total: number; }
+
+  constructor( data: RawEmpireData ) {
+    this._systems_controlled = data.systems_controlled;
+    this._pilots = data.pilots;
+    this._vicory_points = data.victory_points;
+    this._kills = data.kills;
+  }
+
+  get( type: string ): number {
+    return 0;
   }
 }
