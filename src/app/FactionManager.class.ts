b39493a6cfa47d4e4086e0e0c8ecece7befeace8
Rob Monhemius <robmonhemius@gmail.com>
2021-07-05 22:41:00 +0200

    moved factionmanager

---
+++ src/app/FactionManager.class.ts
@@ -0,0 +1,38 @@
+import { BehaviorSubject } from "rxjs";
+import { AmarrFaction, CaldariFaction, Faction, GallenteFaction, MinmatarFaction } from "./Faction.class";
+import { RawEmpireData } from "./fw-empires.service";
+
+export class FactionManager {
+  public factions: Faction[];
+  public update$: BehaviorSubject<null> = new BehaviorSubject( null );
+
+  constructor( raw_data: RawEmpireData[] ) {
+    this.factions = raw_data.map( raw_data => this.init_faction( raw_data ) as Faction );
+    this.update$.next(null);
+  }
+
+  private init_faction( raw_data: RawEmpireData ): Faction | undefined {
+    switch (raw_data.faction_id) {
+      case 500001:
+        return new CaldariFaction( raw_data );
+      case 500002:
+        return new MinmatarFaction( raw_data );
+      case 500003:
+        return new AmarrFaction( raw_data );
+      case 500004:
+        return new GallenteFaction( raw_data );
+      default:
+        return undefined;
+    }
+  }
+
+  public toggle( name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' ) {
+    const faction = this.find( name );
+    faction.enabled = !faction.enabled;
+    this.update$.next( null );
+  }
+
+  private find( name : 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' ) {
+    return this.factions.find( faction => faction.name === name ) as Faction;
+  }
+}
--- src/app/fw-empires.service.ts
+++ src/app/fw-empires.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { EveHttpService } from './eve-http.service';
-import { AmarrFaction, CaldariFaction, Faction, GallenteFaction, MinmatarFaction } from './Faction.class';
+import { Faction } from './Faction.class';
+import { FactionManager } from './FactionManager.class';
 import { ChartData } from './interfaces/ChartData.interface';
 
 @Injectable({
@@ -72,51 +73,6 @@ export class FwEmpiresService {
   }
 
 }
-
-
-
-
-
-
-
-
-class FactionManager {
-  public factions: Faction[];
-  public update$: BehaviorSubject<null> = new BehaviorSubject( null );
-
-  constructor( raw_data: RawEmpireData[] ) {
-    this.factions = raw_data.map( raw_data => this.init_faction( raw_data ) as Faction );
-    this.update$.next(null);
-  }
-
-  private init_faction( raw_data: RawEmpireData ): Faction | undefined {
-    switch (raw_data.faction_id) {
-      case 500001:
-        return new CaldariFaction( raw_data );
-      case 500002:
-        return new MinmatarFaction( raw_data );
-      case 500003:
-        return new AmarrFaction( raw_data );
-      case 500004:
-        return new GallenteFaction( raw_data );
-      default:
-        return undefined;
-    }
-  }
-
-  public toggle( name: 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' ) {
-    const faction = this.find( name );
-    faction.enabled = !faction.enabled;
-    this.update$.next( null );
-  }
-
-  private find( name : 'Minmatar' | 'Amarr' | 'Caldari' | 'Gallente' ) {
-    return this.factions.find( faction => faction.name === name ) as Faction;
-  }
-}
-
-
-
 
 export interface RawEmpireData {
   faction_id: number;
